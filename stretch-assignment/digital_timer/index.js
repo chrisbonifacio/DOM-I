59c2d02e9647eee07ee0ef94568a8c4d877840c9
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-12-08 13:19:31 -0500

    functionality for timer. counts down from 10 sec.

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,53 @@
+// tens
+const secondTens = document.getElementById("secondTens")
+
+// ones
+const secondOnes = document.getElementById("secondOnes")
+
+// colon
+const colon = document.getElementById("colon")
+
+// ms hundreds
+const msHundreds = document.getElementById("msHundreds")
+
+// ms tens
+const msTens = document.getElementById("msTens")
+
+// numbers
+let st = 0
+let so = 9
+let msh = 9
+let mst = 9
+
+setInterval(() => {
+  if (mst <= 0) {
+    if (st === 0 && so === 0 && msh === 0) {
+      mst = 0
+    } else {
+      mst = 9
+
+      if (msh <= 0) {
+        msh = 9
+        if (so <= 0) {
+          so = 9
+          if (st <= 0) {
+            st = 5
+          } else {
+            st--
+          }
+        } else {
+          so--
+        }
+      } else {
+        msh--
+      }
+    }
+  } else {
+    mst--
+  }
+
+  secondTens.textContent = st
+  secondOnes.textContent = so
+  msHundreds.textContent = msh
+  msTens.textContent = mst
+}, 10)
